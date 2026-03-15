8f5b0bd73cdeba034543216aee887fb011670163
AjithSankar <mailajithkumars@gmail.com>
2026-03-16 00:08:12 +0530

    Implemented Chat Memory Persistence

--- src/main/java/dev/ak/ai/config/MemoryConfig.java
+++ src/main/java/dev/ak/ai/config/MemoryConfig.java
@@ -4,15 +4,25 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.memory.repository.jdbc.PostgresChatMemoryRepositoryDialect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class MemoryConfig {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Bean
     public ChatMemoryRepository chatMemoryRepository() {
-        return new InMemoryChatMemoryRepository();
+        return JdbcChatMemoryRepository.builder()
+                .jdbcTemplate(jdbcTemplate)
+                .dialect(new PostgresChatMemoryRepositoryDialect())
+                .build();
     }
 
     @Bean
